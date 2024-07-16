919c97ca24c508b97a02b7e55f8f7e697188676a
kyle-park-io <andy3638@naver.com>
2024-07-16 17:13:33 +0900

    feat: add functionality to extract top 10 files by commit time

--- scripts/cron-sort-blog.sh
+++ scripts/cron-sort-blog.sh
@@ -3,6 +3,7 @@
 mkdir -p sort
 
 output_file="sort/sorted_md_files.txt"
+output_10_file="sort/sorted_md_10_files.txt"
 
 echo "[" >$output_file
 
@@ -10,8 +11,18 @@ git ls-files md | while read file; do
     last_commit_date=$(git log -1 --format="%ci" -- $file)
     filename=$(basename "$file")
     echo "$last_commit_date $filename"
-done | sort | awk '{print "\""$4"\","}' >>$output_file
+done | sort -r | awk '{print "\""$4"\","}' >>$output_file
 
 echo "]" >>$output_file
+
+echo "[" >$output_10_file
+
+git ls-files md | while read file; do
+    last_commit_date=$(git log -1 --format="%ci" -- $file)
+    filename=$(basename "$file")
+    echo "$last_commit_date $filename"
+done | sort -r | head -n 10 | awk '{print "\""$4"\","}' >>$output_10_file
+
+echo "]" >>$output_10_file
 
 cp -r sort /usr/src/app
